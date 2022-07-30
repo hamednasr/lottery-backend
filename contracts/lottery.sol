54944fe17a2abbfb2a739e5b18011e067725e291
hamed nasr <hmd.nasr@gmail.com>
2022-07-30 05:50:49 -0400

    random number 1

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,25 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-lottery__fewETH();
 
-contract lottery{
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
+error lottery__NotEnoughETH();
+
+contract lottery is VRFConsumerBaseV2{
+
+    // state variables
     uint private immutable i_minFee;
+    address payable[] private s_players;
 
-    constructor (uint minFee) {
+    // Events
+    event LotteryEnter(address indexed player);
+
+    constructor (address vrfCoordinatorV2, uint minFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_minFee = minFee
     }
 
     function enterLottery()  returns () {
-        if (msg.value < i_minFee) revert lottery__fewETH();
+        if (msg.value < i_minFee) revert lottery__NotEnoughETH();
+        s_players.push(payable(msg.sender));
+
+        emit LotteryEnter(msg.sender);
+
     }
 
-    function pichWinner() {
+    function requestRandomWinner() external{
+              
+    }
 
+    function fulfillRandomWords(uint requestId, uint[] memory randomWords) internal override {
+        
     }
 
     function getEntranceFee() public view returns (uint) {
         return i_minFee;
     }
+
+    function getPlayers(uint index) public view returns (address) {
+        return s_players[index]
+    }
+
 }
